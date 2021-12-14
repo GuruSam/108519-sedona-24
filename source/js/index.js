709a4ebe828261d2c5b6c8e4b61a4332ad17e156
GuruSam <artem7127.com@inbox.ru>
2021-12-14 19:37:43 +0500

    Nav script fix

--- source/js/index.js
+++ source/js/index.js
@@ -3,10 +3,10 @@ const mainNav = document.querySelector('.main-nav')
 
 navToggle.addEventListener('click', () => {
   if (mainNav.classList.contains('main-nav--closed')) {
-    mainNav.classList.toggle('main-nav--closed')
+    mainNav.classList.remove('main-nav--closed')
     navToggle.classList.add('page-header__nav-toggle--close')
   } else {
-    mainNav.classList.toggle('main-nav--closed')
+    mainNav.classList.add('main-nav--closed')
     navToggle.classList.remove('page-header__nav-toggle--close')
   }
 })
