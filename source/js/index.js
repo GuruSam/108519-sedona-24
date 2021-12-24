bb8c1e524af2a04a5b0a71234e90d49582bef38c
GuruSam <artem7127.com@inbox.ru>
2021-12-24 14:01:01 +0500

    Fixes

--- source/js/index.js
+++ source/js/index.js
@@ -1,12 +1,12 @@
-const navToggle = document.querySelector('.page-header__nav-toggle')
+const navToggle = document.querySelector('.nav-toggle')
 const mainNav = document.querySelector('.main-nav')
 
 navToggle.addEventListener('click', () => {
   if (mainNav.classList.contains('main-nav--closed')) {
     mainNav.classList.remove('main-nav--closed')
-    navToggle.classList.add('page-header__nav-toggle--close')
+    navToggle.classList.add('nav-toggle--close')
   } else {
     mainNav.classList.add('main-nav--closed')
-    navToggle.classList.remove('page-header__nav-toggle--close')
+    navToggle.classList.remove('nav-toggle--close')
   }
 })
